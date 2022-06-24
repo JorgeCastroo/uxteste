1d5e2e1fccc309efe6ba62b44e8f38a9f667b18f
Rodrigo Santos <rolns002@gmail.com>
2022-06-24 10:05:02 -0300

    Correções no código

--- src/modules/solicitacao/screens/SolicitacaoReceivement/index.tsx
+++ src/modules/solicitacao/screens/SolicitacaoReceivement/index.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react'
 import { Alert, View } from 'react-native'
-import { Text } from 'react-native-paper'
 import { StackScreenProps } from '@react-navigation/stack'
 import { SolicitacaoRoutesParams } from '../../interfaces/SolicitacaoRoutesParams'
-import * as S from './styles'
 import themes from '../../../../styles/themes'
-import { elevation } from '../../../../styles/layout'
 import { useAppDispatch, useAppSelector } from '../../../../redux/hooks'
 import { addScannedSolicitacao } from '../../reducers/solicitacaoScan/solicitacaoScanReducer'
 import Render from '../../../../components/Screen/Render'
@@ -14,13 +11,14 @@ import Section from '../../../../components/Screen/Section'
 import SolicitacaoBox from '../../components/SolicitacaoBox'
 import Button from '../../../../components/Button'
 import findLista from '../../scripts/findLista'
-import send from './scripts/send'
 import start from './scripts/start'
+import cancel from './scripts/cancel'
+import send from './scripts/send'
 import { idStatusLista } from '../../../../constants/idStatusLista'
 import findListaPosition from '../../scripts/findListaPosition'
-import cancel from './scripts/cancel'
 import Form from './components/Form'
 import SuccessModal from './components/SuccessModal'
+import StatusBox from './components/StatusBox'
 import { getCoords } from '../../../app/scripts/geolocationService'
 
 const SolicitacaoReceivement: React.FC <StackScreenProps<SolicitacaoRoutesParams, 'solicitacaoReceivement'>> = ({ navigation }) => {
@@ -45,6 +43,21 @@ const SolicitacaoReceivement: React.FC <StackScreenProps<SolicitacaoRoutesParams
         navigation.navigate('solicitacaoScan')
     }
 
+    const handleStart = () => {
+        start(dispatch, !!network, handleNavigate, currentSolicitacao!.idLista, getCoords(location!))
+    }
+
+    const handleCancel = () => {
+        cancel(
+            dispatch, 
+            !!network, 
+            () => navigation.navigate('solicitacaoList'), 
+            userData!, 
+            currentSolicitacao!.idLista, 
+            motivoCancelamento
+        )
+    }
+
     const handleSend = () => {
         send(
             dispatch, 
@@ -59,21 +72,6 @@ const SolicitacaoReceivement: React.FC <StackScreenProps<SolicitacaoRoutesParams
         )
     }
 
-    const handleCancel = () => {
-        cancel(
-            dispatch, 
-            !!network, 
-            () => navigation.navigate('solicitacaoList'), 
-            userData!, 
-            currentSolicitacao!.idLista, 
-            motivoCancelamento
-        )
-    }
-
-    const handleStart = () => {
-        start(dispatch, !!network, handleNavigate, currentSolicitacao!.idLista, getCoords(location!))
-    }
-
     return(
 
         <>
@@ -85,15 +83,15 @@ const SolicitacaoReceivement: React.FC <StackScreenProps<SolicitacaoRoutesParams
                             <SolicitacaoBox {...currentSolicitacao!} position = {findListaPosition(currentSolicitacao, roteirizacao)} />
                         </Section>
                         <Section type = "row" marginTop = {8} between>
-                            <S.Box style = {elevation.elevation2}>
-                                <Text style = {{color: '#333333', fontSize: 22}}>Recebidos</Text>
-                                <Text style = {{marginTop: 20, color: themes.status.success.primary, fontSize: 32, fontWeight: 'bold'}}>{findLista(lista!, currentSolicitacao!.idLista).listaVolumes.filter(f => f.dtLeituraFirstMile.length > 1).length}</Text>
-                            </S.Box>
+                            <StatusBox
+                                theme = {themes.status.success.primary}
+                                text = {findLista(lista!, currentSolicitacao!.idLista).listaVolumes.filter(f => f.dtLeituraFirstMile.length > 1).length}
+                            />
                             <View style = {{marginRight: 20}} />
-                            <S.Box style = {elevation.elevation2}>
-                                <Text style = {{color: '#333333', fontSize: 22}}>Pendentes</Text>
-                                <Text style = {{marginTop: 20, color: themes.status.error.primary, fontSize: 32, fontWeight: 'bold'}}>{findLista(lista!, currentSolicitacao!.idLista).listaVolumes.filter(f => f.dtLeituraFirstMile === '').length}</Text>
-                            </S.Box>
+                            <StatusBox
+                                theme = {themes.status.error.primary}
+                                text = {findLista(lista!, currentSolicitacao!.idLista).listaVolumes.filter(f => f.dtLeituraFirstMile === '').length}
+                            />
                         </Section>
                         <Section marginTop = {40}>
                             {[2].includes(currentSolicitacao.situacao) && (
@@ -144,7 +142,9 @@ const SolicitacaoReceivement: React.FC <StackScreenProps<SolicitacaoRoutesParams
                                         onPress = {() => {
                                             const current = findLista(lista!, currentSolicitacao!.idLista)
                                             if(!current.listaVolumes.some(f => f.dtLeituraFirstMile.length > 1)){
-                                                Alert.alert('Atenção', 'Não é possível finalizar o recebimento da lista sem escanear todos os volumes!', [{ text: 'Ok' }])
+                                                Alert.alert('Atenção', 'Não é possível finalizar o recebimento da lista sem escanear todos os volumes!', [
+                                                    { text: 'Ok' }
+                                                ])
                                             }else handleSend()
                                         }}
                                     />
