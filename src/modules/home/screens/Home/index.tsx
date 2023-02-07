aea244bce590d6347c69727c3bd8342ba00a1f9d
jorgecastro <jorgeeduardo27007@gmail.com>
2023-02-07 14:27:42 -0300

    Tratamento de quando esta sem internt

--- src/modules/home/screens/Home/index.tsx
+++ src/modules/home/screens/Home/index.tsx
@@ -1,99 +1,128 @@
-import React, { useEffect, useState } from 'react'
-import { Text } from 'react-native-paper'
-import { useIsFocused } from '@react-navigation/native'
-import { interval } from 'rxjs'
-import { useObservable } from 'beautiful-react-hooks'
-import themes from '../../../../styles/themes'
-import { useAppDispatch, useAppSelector } from '../../../../redux/hooks'
-import Render from '../../../../components/Screen/Render'
-import HomeHeader from '../../components/HomeHeader'
-import Section from '../../../../components/Screen/Section'
-import TopBox from '../../components/TopBox'
-import GroupStatus from '../../components/Group/Status'
-import HomeMessage from '../../components/Message'
-import SkeletonHomeMessage from '../../components/Message/Skeleton'
-import Container from '../../../../components/Container'
-import sendAppLocation from '../../../app/scripts/requests/sendAppLocation'
-import { getGeolocation } from '../../../app/scripts/geolocationService'
-import initPushNotification from '../../../app/scripts/pushNotification/initPushNotification'
-import AppVersion from '../../../app/components/AppVersion'
-import getColetas from '../../../coletas/scripts/getColetas'
-import dayMoment from '../../../../utils/dayMoment'
-import getRemainder from '../../../../utils/getRemainder'
-import checkListaUpdate from '../../../solicitacao/scripts/checkListaUpdate'
+import React, {useEffect, useState} from 'react';
+import {Text} from 'react-native-paper';
+import {useIsFocused} from '@react-navigation/native';
+import {interval} from 'rxjs';
+import {useObservable} from 'beautiful-react-hooks';
+import themes from '../../../../styles/themes';
+import {useAppDispatch, useAppSelector} from '../../../../redux/hooks';
+import Render from '../../../../components/Screen/Render';
+import HomeHeader from '../../components/HomeHeader';
+import Section from '../../../../components/Screen/Section';
+import TopBox from '../../components/TopBox';
+import GroupStatus from '../../components/Group/Status';
+import HomeMessage from '../../components/Message';
+import SkeletonHomeMessage from '../../components/Message/Skeleton';
+import Container from '../../../../components/Container';
+import sendAppLocation from '../../../app/scripts/requests/sendAppLocation';
+import {getGeolocation} from '../../../app/scripts/geolocationService';
+import initPushNotification from '../../../app/scripts/pushNotification/initPushNotification';
+import AppVersion from '../../../app/components/AppVersion';
+import getColetas from '../../../coletas/scripts/getColetas';
+import dayMoment from '../../../../utils/dayMoment';
+import getRemainder from '../../../../utils/getRemainder';
+import checkListaUpdate from '../../../solicitacao/scripts/checkListaUpdate';
+import Button from '../../../../components/Button';
+import {useNetInfo} from '@react-native-community/netinfo';
 
-const requestInterval = interval(1000)
+const requestInterval = interval(1000);
 
 const Home: React.FC = () => {
+  const netInfo = useNetInfo();
+  const dispatch = useAppDispatch();
+  const {location} = useAppSelector(s => s.app);
+  const {userData} = useAppSelector(s => s.auth);
+  const {lista} = useAppSelector(s => s.lista);
+  const {coletas} = useAppSelector(s => s.coletas);
+  //const { roteirizacao } = useAppSelector(s => s.roteirizacao)
+  const {requestColeta} = useAppSelector(s => s.requestColetas);
+  const isFocused = useIsFocused();
 
-    const dispatch = useAppDispatch()
-    const { location } = useAppSelector(s => s.app)
-    const { userData } = useAppSelector(s => s.auth)
-    const { lista } = useAppSelector(s => s.lista)
-    const { coletas } = useAppSelector(s => s.coletas)
-    //const { roteirizacao } = useAppSelector(s => s.roteirizacao)
-    const { requestColeta } = useAppSelector(s => s.requestColetas)
-    const isFocused = useIsFocused()
+  const [seconds, setSeconds] = useState(0);
 
-    const [seconds, setSeconds] = useState(0)
+  const SHOW_COLETAS_LOADING = requestColeta.loading;
+  const SHOW_COLETAS_DATA =
+    !SHOW_COLETAS_LOADING && !!coletas && coletas.length > 0;
+  const SHOW_DATA = !!lista && !!userData;
 
-    const SHOW_COLETAS_LOADING = requestColeta.loading
-    const SHOW_COLETAS_DATA = !SHOW_COLETAS_LOADING && !!coletas && coletas.length > 0
-    const SHOW_DATA = !!lista && !!userData
+  useObservable(requestInterval, setSeconds as any);
 
-    useObservable(requestInterval, setSeconds as any)
+  useEffect(() => {
+    if (userData) initPushNotification(userData.idUsuarioSistema);
+  }, [dispatch, userData]);
 
-    useEffect(() => {
-        if(userData) initPushNotification(userData.idUsuarioSistema)
-    }, [dispatch, userData])
+  useEffect(() => {
+    if (userData && isFocused) getColetas(dispatch, userData);
+  }, [dispatch, userData, isFocused]);
 
-    useEffect(() => {
-        if(userData && isFocused) getColetas(dispatch, userData)
-    }, [dispatch, userData, isFocused])
+  useEffect(() => {
+    if (getRemainder(seconds, 5)) getGeolocation(dispatch);
 
-    useEffect(() => {
-        if(getRemainder(seconds, 5)) getGeolocation(dispatch)  
+    if (
+      getRemainder(seconds, 30) &&
+      SHOW_DATA &&
+      !!location &&
+      !!location.coords
+    ) {
+      sendAppLocation(
+        dispatch,
+        userData,
+        lista.map(i => i.idLista),
+        location.coords,
+      );
+    }
 
-        if(getRemainder(seconds, 30) && SHOW_DATA && !!location && !!location.coords){
-            sendAppLocation(dispatch, userData, lista.map(i => i.idLista), location.coords)
-        }
-        
-        if(getRemainder(seconds, 60) && SHOW_DATA) checkListaUpdate(dispatch, userData)
-    }, [dispatch, seconds, SHOW_DATA])
+    if (getRemainder(seconds, 60) && SHOW_DATA)
+      checkListaUpdate(dispatch, userData);
+  }, [dispatch, seconds, SHOW_DATA]);
 
-    return(
+  return (
+    <>
+      <Render
+        statusBarOptions={{
+          barStyle: 'light-content',
+          backgroundColor: themes.colors.primary,
+        }}
+        align="space-between"
+        paddingBottom={24}
+        onRefresh={async () => await getColetas(dispatch, userData!)}>
+        <Container padding={false}>
+          <HomeHeader />
+          <TopBox />
+          <Section marginTop={30} marginBottom={20}>
+            <Text style={{color: '#333333', fontSize: 18}}>{dayMoment()}</Text>
+            <Text style={{color: '#333333', fontSize: 24, fontWeight: 'bold'}}>
+              {userData?.nomeUsuario ?? 'Usuário'}
+            </Text>
+          </Section>
+          <Section marginBottom={40}>
+            {SHOW_COLETAS_DATA ? (
+              <></>
+            ) : (
+              <Button
+                label={
+                  netInfo.isInternetReachable
+                    ? 'Atualizar'
+                    : 'Sem conexão com a internet'
+                }
+                marginHorizontal={true}
+                disabled={!netInfo.isInternetReachable}
+                onPress={() => getColetas(dispatch, userData!)}
+              />
+            )}
+            {SHOW_COLETAS_LOADING && <SkeletonHomeMessage />}
+            {SHOW_COLETAS_DATA && <HomeMessage />}
+          </Section>
+          {SHOW_DATA && (
+            <>
+              {/* <GroupInfo /> */}
+              <GroupStatus />
+            </>
+          )}
+        </Container>
+        <AppVersion />
+      </Render>
+    </>
+  );
+};
 
-        <>
-            <Render
-                statusBarOptions = {{ barStyle: 'light-content', backgroundColor: themes.colors.primary }}
-                align = "space-between"
-                paddingBottom = {24}
-                onRefresh = {async () => await getColetas(dispatch, userData!)}
-            >
-                <Container padding = {false}>
-                    <HomeHeader />
-                    <TopBox />
-                    <Section marginTop = {30} marginBottom = {20}>
-                        <Text style = {{color: '#333333', fontSize: 18}}>{dayMoment()}</Text>
-                        <Text style = {{color: '#333333', fontSize: 24, fontWeight: 'bold'}}>{userData?.nomeUsuario ?? 'Usuário'}</Text>
-                    </Section>
-                    <Section marginBottom = {40}>
-                        {SHOW_COLETAS_LOADING && <SkeletonHomeMessage />}
-                        {SHOW_COLETAS_DATA && <HomeMessage />}
-                    </Section>
-                    {SHOW_DATA && (
-                        <>
-                            {/* <GroupInfo /> */}
-                            <GroupStatus />
-                        </>
-                    )}
-                </Container>
-                <AppVersion />
-            </Render>
-        </>
-
-    )
-
-}
-
-export default Home
+export default Home;
--- src/modules/solicitacao/screens/SolicitacaoList/index.tsx
+++ src/modules/solicitacao/screens/SolicitacaoList/index.tsx
@@ -1,133 +1,173 @@
-import React, { useEffect, useState } from 'react'
-import { Alert, Text } from 'react-native'
-import { StackScreenProps } from '@react-navigation/stack'
-import { SolicitacaoRoutesParams } from '../../interfaces/SolicitacaoRoutesParams'
-import { Endereco } from '../../interfaces/Lista'
-import themes from '../../../../styles/themes'
-import { useAppDispatch, useAppSelector } from '../../../../redux/hooks'
-import { setCurrentLista, setCurrentSolicitacao } from '../../reducers/lista/listaReducer'
-import { resetScannedSolicitacoes } from '../../reducers/solicitacaoScan/solicitacaoScanReducer'
-import Header from '../../../../components/Screen/Header'
-import Render from '../../../../components/Screen/Render'
-import Section from '../../../../components/Screen/Section'
-import SolicitacaoBox from '../../components/SolicitacaoBox'
-import NoData from '../../../../components/NoData'
-import SolicitacaoListSearchbar from './components/Searchbar'
-import FormError from '../../../../components/Form/Error'
-import Button from '../../../../components/Button'
-import Loader from './components/Loader'
-import localGetLista from '../../scripts/local/localGetLista'
-import { idStatusLista } from '../../../../constants/idStatusLista'
-import { syncValuesLista } from '../../scripts/sync'
-import getAddresses from '../../scripts/getAddresses'
-import findLista from '../../scripts/findLista'
-import closeLista from '../../scripts/requests/requestCloseLista'
+import React, {useEffect, useState} from 'react';
+import {Alert, Text} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
+import {SolicitacaoRoutesParams} from '../../interfaces/SolicitacaoRoutesParams';
+import {Endereco} from '../../interfaces/Lista';
+import themes from '../../../../styles/themes';
+import {useAppDispatch, useAppSelector} from '../../../../redux/hooks';
+import {
+  setCurrentLista,
+  setCurrentSolicitacao,
+} from '../../reducers/lista/listaReducer';
+import {resetScannedSolicitacoes} from '../../reducers/solicitacaoScan/solicitacaoScanReducer';
+import Header from '../../../../components/Screen/Header';
+import Render from '../../../../components/Screen/Render';
+import Section from '../../../../components/Screen/Section';
+import SolicitacaoBox from '../../components/SolicitacaoBox';
+import NoData from '../../../../components/NoData';
+import SolicitacaoListSearchbar from './components/Searchbar';
+import FormError from '../../../../components/Form/Error';
+import Button from '../../../../components/Button';
+import Loader from './components/Loader';
+import localGetLista from '../../scripts/local/localGetLista';
+import {idStatusLista} from '../../../../constants/idStatusLista';
+import {syncValuesLista} from '../../scripts/sync';
+import getAddresses from '../../scripts/getAddresses';
+import findLista from '../../scripts/findLista';
+import closeLista from '../../scripts/requests/requestCloseLista';
 
-const SolicitacaoList: React.FC<StackScreenProps<SolicitacaoRoutesParams, 'solicitacaoList'>> = ({ navigation }) => {
+const SolicitacaoList: React.FC<
+  StackScreenProps<SolicitacaoRoutesParams, 'solicitacaoList'>
+> = ({navigation}) => {
+  const dispatch = useAppDispatch();
+  const {lista, filteredEnderecos, loadingNewLista} = useAppSelector(
+    s => s.lista,
+  );
+  const {userData} = useAppSelector(s => s.auth);
+  const {dtUltimaAtualizacao} = useAppSelector(s => s.app);
 
-    const dispatch = useAppDispatch()
-    const { lista, filteredEnderecos, loadingNewLista } = useAppSelector(s => s.lista)
-    const { userData } = useAppSelector(s => s.auth)
-    const { dtUltimaAtualizacao } = useAppSelector(s => s.app)
+  //const { roteirizacao } = useAppSelector(s => s.roteirizacao)
+  //const { requestGetRoteirizacao } = useAppSelector(s => s.requestRoteirizacao)
+  const {requestGetLista, requestCloseLista} = useAppSelector(
+    s => s.requestLista,
+  );
 
-    //const { roteirizacao } = useAppSelector(s => s.roteirizacao)
-    //const { requestGetRoteirizacao } = useAppSelector(s => s.requestRoteirizacao)
-    const { requestGetLista, requestCloseLista } = useAppSelector(s => s.requestLista)
+  const [allIsSync, setAllIsSync] = useState(true);
 
-    const [allIsSync, setAllIsSync] = useState(true)
+  const SHOW_LOADING = loadingNewLista;
+  const SHOW_NO_DATA = !SHOW_LOADING && !lista;
+  const SHOW_DATA = !SHOW_LOADING && !!lista && lista.length > 0;
 
-    const SHOW_LOADING = loadingNewLista
-    const SHOW_NO_DATA = !SHOW_LOADING && !lista
-    const SHOW_DATA = !SHOW_LOADING && !!lista && lista.length > 0
+  const SHOW_FILTERED_LISTA_DATA = !SHOW_LOADING && !!filteredEnderecos;
+  const SHOW_FILTERED_LISTA_NO_DATA =
+    !SHOW_LOADING && !!filteredEnderecos && filteredEnderecos.length === 0;
 
-    const SHOW_FILTERED_LISTA_DATA = !SHOW_LOADING && !!filteredEnderecos
-    const SHOW_FILTERED_LISTA_NO_DATA = !SHOW_LOADING && !!filteredEnderecos && filteredEnderecos.length === 0
+  const SHOW_LISTA_DATA = !SHOW_LOADING && !SHOW_FILTERED_LISTA_DATA && !!lista;
+  const SHOW_LISTA_NO_DATA =
+    !SHOW_LOADING && !SHOW_FILTERED_LISTA_DATA && !!lista && lista.length === 0;
 
-    const SHOW_LISTA_DATA = !SHOW_LOADING && !SHOW_FILTERED_LISTA_DATA && !!lista
-    const SHOW_LISTA_NO_DATA = !SHOW_LOADING && !SHOW_FILTERED_LISTA_DATA && !!lista && lista.length === 0
+  const loaderPercent = requestGetLista.data ? 100 : 0;
 
-    const loaderPercent = requestGetLista.data ? 100 : 0
+  const handleNavigate = (item: Endereco) => {
+    dispatch(setCurrentLista(findLista(lista!, item.idLista)));
+    dispatch(setCurrentSolicitacao(item));
+    dispatch(resetScannedSolicitacoes());
+    navigation.navigate('solicitacaoReceivement');
+  };
 
-    const handleNavigate = (item: Endereco) => {
-        dispatch(setCurrentLista(findLista(lista!, item.idLista)))
-        dispatch(setCurrentSolicitacao(item))
-        dispatch(resetScannedSolicitacoes())
-        navigation.navigate('solicitacaoReceivement')
-    }
-    
-    useEffect(() => {
-        (async() => {
-            setAllIsSync(await syncValuesLista())
-        })()
-    }, [dispatch, lista])
+  useEffect(() => {
+    (async () => {
+      setAllIsSync(await syncValuesLista());
+    })();
+  }, [dispatch, lista]);
 
-    return (
-
-        <>
-            <Render 
-                statusBarOptions = {{barStyle: 'light-content', backgroundColor: themes.colors.primary}} 
-                paddingBottom = {20} 
-                align = {SHOW_LOADING ? 'space-between' : undefined}
-                onRefresh = {async () => await localGetLista(dispatch)}
-            >
-                <Header title = "Listas" screenName = "solicitacaoList" goBack = {false} />
-                {SHOW_LOADING && <Loader percent = {loaderPercent} />}
-                {SHOW_NO_DATA && <NoData emoji = "confused" message = {['Você não possui listas!']} />}
-                <FormError
-                    visible = {!allIsSync}
-                    marginTop = {24}
-                    message = "Ainda faltam listas para sincronizar!"
+  return (
+    <>
+      <Render
+        statusBarOptions={{
+          barStyle: 'light-content',
+          backgroundColor: themes.colors.primary,
+        }}
+        paddingBottom={20}
+        align={SHOW_LOADING ? 'space-between' : undefined}
+        onRefresh={async () => await localGetLista(dispatch)}>
+        <Header title="Listas" screenName="solicitacaoList" goBack={false} />
+        {SHOW_LOADING && <Loader percent={loaderPercent} />}
+        {SHOW_NO_DATA && (
+          <NoData emoji="confused" message={['Você não possui listas!']} />
+        )}
+        <FormError
+          visible={!allIsSync}
+          marginTop={24}
+          message="Ainda faltam listas para sincronizar!"
+        />
+        {SHOW_DATA && (
+          <>
+            <Section marginTop={10} marginBottom={0}>
+              <Text style={{color: '#333333', fontSize: 16}}>
+                Última atualização: {dtUltimaAtualizacao}
+              </Text>
+            </Section>
+            <SolicitacaoListSearchbar />
+            <Section marginTop={10}>
+              {SHOW_FILTERED_LISTA_NO_DATA && (
+                <NoData
+                  emoji="confused"
+                  message={['Nenhum endereço encontrado!']}
                 />
-                {SHOW_DATA && (
-                    <>
-                        <Section marginTop = {10} marginBottom = {0}>
-                            <Text style = {{color: '#333333', fontSize: 16}}>Última atualização: {dtUltimaAtualizacao}</Text>
-                        </Section>
-                        <SolicitacaoListSearchbar />
-                        <Section marginTop = {10}>
-                            {SHOW_FILTERED_LISTA_NO_DATA && <NoData emoji = "confused" message = {['Nenhum endereço encontrado!']} />}
-                            {SHOW_FILTERED_LISTA_DATA && filteredEnderecos.map((item, index) => (
-                                <SolicitacaoBox 
-                                    {...item} 
-                                    key = {index} 
-                                    //position = {findListaPosition(item, roteirizacao)}
-                                    onPress = {() => handleNavigate(item)} 
-                                />
-                            ))} 
+              )}
+              {SHOW_FILTERED_LISTA_DATA &&
+                filteredEnderecos.map((item, index) => (
+                  <SolicitacaoBox
+                    {...item}
+                    key={index}
+                    //position = {findListaPosition(item, roteirizacao)}
+                    onPress={() => handleNavigate(item)}
+                  />
+                ))}
 
-                            {SHOW_LISTA_NO_DATA && <NoData emoji = "confused" message = {['Nenhum endereço em aberto!']} />}
-                            {SHOW_LISTA_DATA && getAddresses(lista).map((item, index) => (
-                                <SolicitacaoBox 
-                                    {...item} 
-                                    key = {index} 
-                                    //position = {findListaPosition(item, roteirizacao)}
-                                    onPress = {() => handleNavigate(item)} 
-                                />
-                            ))}
-                        </Section>
-                        <Section>
-                            <Button
-                                label = "Finalizar Rota"
-                                color = {themes.gradient.success}
-                                marginHorizontal
-                                disabled = {requestCloseLista.loading || !lista.every(f => [idStatusLista['FINALIZADO'], idStatusLista['CANCELADO']].includes(f.situacao))}
-                                loading = {requestCloseLista.loading}
-                                onPress = {() => {
-                                    Alert.alert('Atenção', 'Deseja finalizar a rota?', [
-                                        {text: 'Não', style: 'cancel'},
-                                        {text: 'Sim', onPress: () => closeLista(dispatch, lista.map(f => f.idLista))}
-                                    ])
-                                }}
-                            />
-                        </Section>
-                    </>
-                )}
-                <Section />
-            </Render>
-        </>
+              {SHOW_LISTA_NO_DATA && (
+                <NoData
+                  emoji="confused"
+                  message={['Nenhum endereço em aberto!']}
+                />
+              )}
+              {SHOW_LISTA_DATA &&
+                getAddresses(lista).map((item, index) => (
+                  <SolicitacaoBox
+                    {...item}
+                    key={index}
+                    //position = {findListaPosition(item, roteirizacao)}
+                    onPress={() => handleNavigate(item)}
+                  />
+                ))}
+            </Section>
+            <Section>
+              <Button
+                label="Finalizar Rota"
+                color={themes.gradient.success}
+                marginHorizontal
+                disabled={
+                  requestCloseLista.loading ||
+                  !lista.every(f =>
+                    [
+                      idStatusLista['FINALIZADO'],
+                      idStatusLista['CANCELADO'],
+                    ].includes(f.situacao),
+                  )
+                }
+                loading={requestCloseLista.loading}
+                onPress={() => {
+                  Alert.alert('Atenção', 'Deseja finalizar a rota?', [
+                    {text: 'Não', style: 'cancel'},
+                    {
+                      text: 'Sim',
+                      onPress: () =>
+                        closeLista(
+                          dispatch,
+                          lista.map(f => f.idLista),
+                        ),
+                    },
+                  ]);
+                }}
+              />
+            </Section>
+          </>
+        )}
+        <Section />
+      </Render>
+    </>
+  );
+};
 
-    )
-
-}
-
-export default SolicitacaoList
+export default SolicitacaoList;
